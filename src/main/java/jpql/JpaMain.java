40e9a18c103bdd45feb0cbb31bc02e1a2098979e
Hongsugyeong <olivia258@naver.com>
2023-12-27 19:30:56 +0900

    [Feat] 프로젝션(SELECT) 공부

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,20 +15,20 @@ public class JpaMain {
 
         try {
 
-           Member member = new Member();
-           member.setUsername("member1");
-           member.setAge(10);
-           em.persist(member);
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query.setParameter("username","member1");
-            Member result = query.getSingleResult();
+            em.flush();
+            em.clear();
 
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m",MemberDTO.class)
+                    .getResultList();
 
-
-            System.out.println(result.getUsername());
-
-
+            MemberDTO memberDTO = result.get(0);
+            System.out.println(memberDTO.getAge());
+            System.out.println(memberDTO.getUsername());
 
 
             tx.commit();
