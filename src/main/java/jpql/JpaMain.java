a4a42792ef28521ce1e737de8283b34493c2a31c
Hongsugyeong <olivia258@naver.com>
2023-12-27 19:11:32 +0900

    [Feat] 기본 문법과 쿼리 API 공부

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,44 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em =  emf.createEntityManager();
+
+        EntityTransaction tx =  em.getTransaction();
+        tx.begin();
+
+        try {
+
+           Member member = new Member();
+           member.setUsername("member1");
+           member.setAge(10);
+           em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query.setParameter("username","member1");
+            Member result = query.getSingleResult();
+
+
+
+            System.out.println(result.getUsername());
+
+
+
+
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
