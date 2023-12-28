59f3d4f0e7b4f8f531963134d987e898d39c2ba3
Hongsugyeong <olivia258@naver.com>
2023-12-29 00:19:56 +0900

    [Feat] 경로 표현식 공부

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -15,23 +16,34 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member = new Member();
             member.setUsername("관리자1");
+            member.setTeam(team);
             em.persist(member);
 
             Member member1 = new Member();
             member1.setUsername("관리자2");
+            member1.setTeam(team);
             em.persist(member1);
 
             em.flush();
             em.clear();
 
-            String query =
-                    "select group_concat(m.username) From Member m";
-            List<String> result = em.createQuery(query, String.class)
-                            .getResultList();
+            /*
+                가급적 묵시적 조인 대신에 명시적 조인 사용
+                조인은 SQL 튜닝에 중요 포인트
+                묵시적 조인은 조인이 일어나는 상황을 한눈에 파악하기 어려움
+             */
 
-            for(String s : result) {
+            String query =
+                    "select m.username From Team t join t.members m";
+            List<String> result = em.createQuery(query, String.class)
+                    .getResultList();
+
+            for(Object s : result) {
                 System.out.println(s);
             }
 
